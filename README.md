ca2fda93762f0dcee1872adafd26585a8e1f39a1
Fafnirical <wade.burelbach@gmail.com>
2014-06-15 00:11:32 -0400

    Table maker v2
    Has both subID and appID functionality now; still command-line (it
    doesn't parse the HTML of the actual Steam page)

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-reddit-steamsales
-=================
-
-Table generator (in Reddit comment format) for Steam sales
+Steam Sale Table Generator<br />(in Reddit comment format)
 =================
 
 ## How to use
 
-1. Determine the appIDs you want to use
+1. Determine the appIDs (.../app/*) or packageIDs (.../sub/*) you want to use
 2. Run the Python script:
 
-    <pre><b>maketable.py</b> &lt;<i>outputfile</i>&gt; &lt;<i>appid1</i>&gt; [<i>appid2</i>...]</pre>
+    <pre><b>maketable.py</b> &lt;<i>outputfile</i>&gt; &lt;<i>id1</i>&gt; [<i>id2</i>...]</pre>
--- maketable.py
+++ maketable.py
@@ -1,5 +1,6 @@
 import sys
 import urllib2, json
+import pprint
 
 cc = ['us', 'it', 'uk', 'au']
 currency = {
@@ -20,6 +21,37 @@ with open(filename, "w") as myfile:
 
 appids = sys.argv[2:]
 
+def sub_get_info(table, idx1, country, idx2, subid):
+	response = urllib2.urlopen('http://store.steampowered.com/api/packagedetails?packageids=' + str(subid) + '&cc='+country)
+	data_orig = json.load(response)
+	
+	data = data_orig[str(subid)]['data']
+	sub_appid = subid
+	
+	if('name' in data):
+		table[3 + idx2][0] = '[' + data['name'] + '](http://store.steampowered.com/sub/' + str(subid) + '/)'
+	
+	if('price' in data):
+		pricedata = data['price']
+		table[3 + idx2][1] = str(pricedata['discount_percent']) + '%'
+		table[3 + idx2][2 + idx1] = currency[country] + str(float(pricedata['final']) / 100)
+	
+	if('apps' in data):
+		sub_appid = data['apps'][0]['id']
+	
+	return (table, sub_appid)
+
+def app_get_info(table, idx1, country, idx2, appid, data):
+	if('name' in data):
+		table[3 + idx2][0] = '[' + data['name'] + '](http://store.steampowered.com/app/' + str(appid) + '/)'
+	
+	if('price_overview' in data):
+		pricedata = data['price_overview']
+		table[3 + idx2][1] = str(pricedata['discount_percent']) + '%'
+		table[3 + idx2][2 + idx1] = currency[country] + str(float(pricedata['final']) / 100)
+	
+	return table
+
 def get_table(appids): # get a table with the appIDs
 	# initialize the table
 	table = [['' for x in xrange(10)] for x in xrange(len(appids)+3)]
@@ -34,22 +66,22 @@ def get_table(appids): # get a table with the appIDs
 		data_orig = json.load(response)
 
 		for idx2, appid in enumerate(appids):
+			sub_appid = None
+			
 			if(data_orig[str(appid)]['success'] == False):
-				print 'Invalid AppID: ' + str(appid)
-				continue
-			data = data_orig[str(appid)]['data']
-
-			if('price_overview' in data):
-				pricedata = data['price_overview']
-				table[3 + idx2][2 + idx1] = currency[country] + str(float(pricedata['final']) / 100)
-
+				(table, sub_appid) = sub_get_info(table, idx1, country, idx2, appid)
+				
+				appid = sub_appid
+				
+				response = urllib2.urlopen('http://store.steampowered.com/api/appdetails?appids=' + str(appid) + '&cc='+country)
+				data_orig = json.load(response)
+				
+				data = data_orig[str(appid)]['data']
+			else:
+				data = data_orig[str(appid)]['data']
+				table = app_get_info(table, idx1, country, idx2, appid, data)
+			
 			if(idx1 == 0):
-				if('name' in data):
-					table[3 + idx2][0] = data['name']
-				
-				if('price_overview' in data):
-					table[3 + idx2][1] = str(pricedata['discount_percent']) + '%'
-				
 				if('metacritic' in data):
 					metacritic = data['metacritic']
 					table[3 + idx2][6] = '[' + str(metacritic['score']) + '](' + metacritic['url'] + ')'
@@ -70,20 +102,20 @@ def get_table(appids): # get a table with the appIDs
 						table[3 + idx2][8] = 'Yes'
 					else:
 						table[3 + idx2][8] = 'No'
-			
-			# PCGW Article
-			pcgw_response = urllib2.urlopen('http://pcgamingwiki.com/wiki/Special:Ask/-5B-5BSteam-20AppID::' + str(appid) + '-5D-5D/format%3Djson')
-			try:
-				pcgw_data = json.load(pcgw_response)
-				if('fullurl' in pcgw_data.get('results', {}).itervalues().next()):
-					table[3 + idx2][9] = '[Yes](' + pcgw_data['results'].itervalues().next()['fullurl'] + ')'
-			except ValueError:
-				table[3 + idx2][9] = 'No'
-	
-	# print the table
-	for line in table:
-		l = "|" + u"|".join(line).encode('utf-8').strip() + "|"
-		with open(filename, "a") as myfile:
-			myfile.write(l+"\n")
+				
+				# PCGW Article
+				pcgw_response = urllib2.urlopen('http://pcgamingwiki.com/wiki/Special:Ask/-5B-5BSteam-20AppID::' + str(appid) + '-5D-5D/format%3Djson')
+				try:
+					pcgw_data = json.load(pcgw_response)
+					if('fullurl' in pcgw_data.get('results', {}).itervalues().next()):
+						table[3 + idx2][9] = '[Yes](' + pcgw_data['results'].itervalues().next()['fullurl'] + ')'
+				except ValueError:
+					table[3 + idx2][9] = 'No'
+	return table
 
-get_table(appids)
+table = get_table(appids)
+# print the table
+for line in table:
+	l = "|" + u"|".join(line).encode('utf-8').strip() + "|"
+	with open(filename, "a") as myfile:
+		myfile.write(l+"\n")
